72f9c252c0cec36c834c97c2cf113e96bc05cfb4
Alex Miller <alex.burr.miller@gmail.com>
2016-01-21 20:16:25 -0500

    Added API engine. Started on api routes.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
+
+  # Mount Engines
+  mount Api::Engine => '/api', as: 'api'
 end
